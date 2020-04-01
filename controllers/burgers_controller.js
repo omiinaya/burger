bd0053bdcd4a2f3a5a77d9f90301de1b44d56817
omiinaya <omiinaya@gmail.com>
2020-04-01 13:52:16 -0400

    succesfully devouring burgers.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -19,7 +19,7 @@ router.get("/", function(req, res) {
 router.post("/api/burgers", function(req, res) {
   burgers.create(
     ["burger_name", "devoured"], 
-    [req.body.burger_name, req.body.devoured], 
+    [req.body.burger_name, "0"], 
     function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
@@ -33,7 +33,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   burgers.update(
     {
-      devoured: req.body.devoured
+      devoured: "1"
     },
     condition,
     function(result) {
